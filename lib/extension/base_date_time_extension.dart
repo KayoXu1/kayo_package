e312636e621a5af7f36c185b100679446f01e3a7
kayoxu <371714752@qq.com>
2025-04-29 14:42:36 +0800

    m

--- lib/extension/base_date_time_extension.dart
+++ lib/extension/base_date_time_extension.dart
@@ -4,11 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:kayo_package/kayo_package.dart';
 
 extension BaseDateTimeExtension on DateTime? {
-  String toTimeStr({String? format, String? defaultTime, bool tz = false}) {
+  String toTimeStr({String? format, String? defaultTime, bool tz = false, bool isUtc = false}) {
     try {
       if (null != this) {
         var seconds = this!.millisecondsSinceEpoch;
         if (tz) {
+          if (isUtc) {
+            seconds = seconds - (DateTime.now().timeZoneOffset.inSeconds * 1000);
+          } else {
+            seconds = seconds - (DateTime.now().timeZoneOffset.inMilliseconds);
+          }
           var replaceAll = BaseTimeUtils.timestampToTimeStr(
               seconds, format: format).replaceAll(" ", "T");
           return "${replaceAll}Z";
