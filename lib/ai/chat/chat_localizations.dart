0a1e2fac4d3ed3f999ae13a69c799d6b60b01617
kayoxu <371714752@qq.com>
2025-06-10 14:43:11 +0800

    m

--- lib/ai/chat/chat_localizations.dart
+++ lib/ai/chat/chat_localizations.dart
@@ -1,54 +1,54 @@
 abstract class ChatLocalizations {
   ///  当前聊天为空，不能新建聊天
-  get noMessagesToStartNewChat;
+  String get noMessagesToStartNewChat;
 
   ///  创建新聊天失败
-  get startNewChatFailed;
+  String get startNewChatFailed;
 
   ///  删除会话
-  get deleteSessionTitle;
+  String get deleteSessionTitle;
 
   ///  确定要删除此会话吗?
-  get deleteSessionConfirm;
+  String get deleteSessionConfirm;
 
   ///  取消
-  get cancel;
+  String get cancel;
 
   ///  删除
-  get delete;
+  String get delete;
 
   /// 删除会话失败
-  get deleteSessionFailed;
+  String get deleteSessionFailed;
 
   /// 加载会话失败
-  get loadSessionFailed;
+  String get loadSessionFailed;
 
   ///  新聊天
-  get newChat;
+  String get newChat;
 
   ///  聊天历史
-  get chatHistory;
+  String get chatHistory;
 
   ///  我的
-  get mine;
+  String get mine;
 
   ///  开始新聊天吧
-  get startNewChatHint;
+  String get startNewChatHint;
 
   /// 输入消息
-  get startNewChatHintInput;
+  String get startNewChatHintInput;
 
   /// 发送消息失败
-  get sendFailed;
+  String get sendFailed;
 
   /// 会话
-  get session;
+  String get session;
 
-  get userLoginExit;
+  String get userLoginExit;
 
-  get aiIconSpeaking => "packages/kayo_package/assets/chat/ai_icon_speaking.png";
+  String get aiIconSpeaking => "packages/kayo_package/assets/chat/ai_icon_speaking.png";
 
-  get aiIcon => "packages/kayo_package/assets/chat/ai_icon.png";
+  String get aiIcon => "packages/kayo_package/assets/chat/ai_icon.png";
 
   bool get showUserProfile => true;
 
